f8088f70a684f6ddc1f8d0556f7a1facf1ba3d25
stupiqplonde <kovalevmihail38@gmail.com>
2026-06-09 08:20:06 +0300

    chat.io app

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ app.use(express.static(__dirname + '/public'));
 const expressServer = app.listen(8000, () => {console.log('http://localhost:8000')});
 
 const io = socketio(expressServer);
+let data = null
 
-io.on('connection', () => {
-    console.log('server is run')
-})
+io.on('connection', (socket) => {
+    console.log('server is run');
+    socket.on('newMessageFromServer', (data) => {
+        io.sockets.emit("newMessageFromClients", data);
+    })
+});
